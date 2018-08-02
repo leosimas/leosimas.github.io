ce722a69f692848a6f5b651e9dd82f571e372f6b
leosimas <leosimasgoncalves@gmail.com>
2018-08-02 15:58:44 -0300

    configuração do font-awesome

--- angular_src/src/app/app.module.ts
+++ angular_src/src/app/app.module.ts
@@ -3,12 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+// Add an icon to the library for convenient access in other components
+library.add(fas, fab);
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
