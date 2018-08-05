53d14d0dc6f0b76e0d3a256967ced2a35e64043a
leosimas <leosimasgoncalves@gmail.com>
2018-08-05 15:53:31 -0300

    adição de routes

--- angular_src/src/app/app.module.ts
+++ angular_src/src/app/app.module.ts
@@ -7,17 +7,23 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
+import { AppRoutingModule } from './/app-routing.module';
+import { HomeComponent } from './views/pages/home/home.component';
+import { ToolbarComponent } from './views/components/toolbar/toolbar.component';
 
 // Add an icon to the library for convenient access in other components
 library.add(fas, fab);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
