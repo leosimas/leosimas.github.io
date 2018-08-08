efcdf0004b2a6660d1facdce4e550b8a5c31813f
leosimas <leosimasgoncalves@gmail.com>
2018-08-08 18:21:00 -0300

    iniciando implementação do menu lateral

--- angular_src/src/app/app.component.ts
+++ angular_src/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ToolbarComponent } from './views/components/toolbar/toolbar.component';
+import { MatDrawer } from '@angular/material';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  @ViewChild('appToolbar') appToolbar: ToolbarComponent;
+  @ViewChild('drawer') drawer: MatDrawer;
+
+  ngOnInit(): void {
+    const me = this;
+    this.appToolbar.onMenuClick = function () {
+      me.drawer.toggle();
+    };
+  }
 }
--- angular_src/src/app/views/components/toolbar/toolbar.component.ts
+++ angular_src/src/app/views/components/toolbar/toolbar.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
 
+  onMenuClick: Function;
+
   constructor() { }
 
   ngOnInit() {
