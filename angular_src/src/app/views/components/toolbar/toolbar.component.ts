727f9f1247fe3cf69beb6be52024cc8a418f8c96
leosimas <leosimasgoncalves@gmail.com>
2018-08-10 13:43:33 -0300

    adição dos items de menu com nova configuração de menu na toolbar

--- angular_src/src/app/views/components/toolbar/toolbar.component.ts
+++ angular_src/src/app/views/components/toolbar/toolbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem, MenuService } from '../../../services/menu.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
@@ -9,11 +11,18 @@ export class ToolbarComponent {
 
   onMenuClick: Function;
   showMenuOptions: boolean;
+  menuItems: Array<MenuItem>;
 
-  constructor() { }
+  constructor(private menuService: MenuService) {
+    this.menuItems = menuService.getItems();
+  }
 
   setShowMenuOptions(show: boolean) {
     this.showMenuOptions = show;
+  }
+
+  onMenuItemClick(item: MenuItem) {
+    this.menuService.navigate(item);
   }
 
 }
