fe9ea5f6ec02ed352fe9409d8b101de9db99fa75
leosimas <leosimasgoncalves@gmail.com>
2018-08-10 14:47:14 -0300

    pagina do curriculo

--- angular_src/src/app/app-routing.module.ts
+++ angular_src/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from 'src/app/views/pages/home/home.component';
 import { ContactComponent } from './views/pages/contact/contact.component';
+import { ResumeComponent } from './views/pages/resume/resume.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'resume', component: ResumeComponent },
   { path: 'contact', component: ContactComponent }
 ];
 
