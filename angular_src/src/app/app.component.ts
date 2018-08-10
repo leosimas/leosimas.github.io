caf2a5c1277e340383997cde47e6106fbb4eb98b
leosimas <leosimasgoncalves@gmail.com>
2018-08-10 13:43:33 -0300

    adição dos items de menu com nova configuração de menu na toolbar

--- angular_src/src/app/app.component.ts
+++ angular_src/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { ToolbarComponent } from './views/components/toolbar/toolbar.component';
 import { MatDrawer } from '@angular/material';
 import { ObservableMedia, MediaChange } from '@angular/flex-layout';
 import { Subscription } from 'rxjs';
+import { MenuService, MenuItem } from './services/menu.service';
 
 @Component({
   selector: 'app-root',
@@ -13,13 +14,13 @@ export class AppComponent implements OnInit, OnDestroy {
   @ViewChild('appToolbar') appToolbar: ToolbarComponent;
   @ViewChild('drawer') drawer: MatDrawer;
 
-  showSideDrawer = true;
-  drawerMode = 'side';
-  drawerHasBackdrop = false;
+  showMenu = true;
 
   watcher: Subscription;
+  menuItems: Array<MenuItem>;
 
-  constructor(media: ObservableMedia) {
+  constructor(media: ObservableMedia, private menuService: MenuService) {
+    this.menuItems = menuService.getItems();
     this.watcher = media.subscribe((change: MediaChange) => {
       console.log(change);
       console.log(change.mqAlias);
@@ -39,24 +40,19 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   setSideDrawerStyle(change: MediaChange) {
-    const showSide = ![ 'xs', 'sm' ].includes( change.mqAlias );
+    const showMenu = [ 'xs', 'sm' ].includes( change.mqAlias );
     if ( this.appToolbar ) {
-      this.appToolbar.setShowMenuOptions(showSide);
+      this.appToolbar.setShowMenuOptions(!showMenu);
     }
-    this.drawerHasBackdrop = !showSide ;
-    if ( showSide ) {
-      this.drawerMode = 'side';
-      if (this.drawer && !this.drawer.opened) {
-        this.drawer.open();
-      }
-    } else {
-      this.drawerMode = 'over';
-      if (this.drawer && this.drawer.opened &&
-        this.showSideDrawer !== showSide ) {
-        this.drawer.close();
-      }
+    const shouldClose = !showMenu && this.showMenu !== showMenu;
+    if ( shouldClose && this.drawer && this.drawer.opened ) {
+      this.drawer.close();
     }
 
-    this.showSideDrawer = showSide;
+    this.showMenu = showMenu;
+  }
+
+  onMenuItemClick(item: MenuItem) {
+    this.menuService.navigate(item);
   }
 }
