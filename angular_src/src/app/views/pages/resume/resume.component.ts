cc98d1edbf4466a6474f220d5b26b4157702a209
Leonardo Simas <leosimasgoncalves@gmail.com>
2018-08-11 13:46:01 -0300

    lista de jobs

--- angular_src/src/app/views/pages/resume/resume.component.ts
+++ angular_src/src/app/views/pages/resume/resume.component.ts
@@ -11,13 +11,15 @@ export class ResumeComponent implements OnInit {
 
   certifications: Array<Certification>;
   formation: Array<Graduation>;
-  roles: any;
+  roles: Array<any>;
+  jobs: Array<any>;
 
   constructor(profile: ProfileService) {
     const p = profile.get();
     this.certifications = p.certifications;
     this.formation = p.formation;
     this.roles = p.roles
+    this.jobs = p.jobs;
   }
 
   ngOnInit() {
