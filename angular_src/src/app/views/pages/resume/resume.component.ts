24387df77a1d3be69d2b819dea2bfe3c866754e8
Leonardo Simas <leosimasgoncalves@gmail.com>
2018-08-11 11:23:52 -0300

    atualização da atuação

--- angular_src/src/app/views/pages/resume/resume.component.ts
+++ angular_src/src/app/views/pages/resume/resume.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Certification } from '../../components/certification-card/certification-card.component';
-import { ProfileService, Graduation } from '../../../services/profile.service';
+import { ProfileService, Graduation, Profile } from '../../../services/profile.service';
 
 @Component({
   selector: 'app-resume',
@@ -11,10 +11,13 @@ export class ResumeComponent implements OnInit {
 
   certifications: Array<Certification>;
   formation: Array<Graduation>;
+  roles: any;
 
   constructor(profile: ProfileService) {
-    this.certifications = profile.get().certifications;
-    this.formation = profile.get().formation;
+    const p = profile.get();
+    this.certifications = p.certifications;
+    this.formation = p.formation;
+    this.roles = p.roles
   }
 
   ngOnInit() {
