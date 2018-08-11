32e379e9049cb0b75b0bd919760bb5b96d8e0257
Leonardo Simas <leosimasgoncalves@gmail.com>
2018-08-11 15:26:59 -0300

    adição de urls das certificações

--- angular_src/src/app/views/components/certification-card/certification-card.component.ts
+++ angular_src/src/app/views/components/certification-card/certification-card.component.ts
@@ -4,13 +4,16 @@ export class Certification {
   title: string;
   description: string = null;
   iconClass: string;
+  url: string;
   
   constructor(title: string,
     description: string,
-    iconClass: string) {
+    iconClass: string,
+    url: string) {
       this.title = title;
       this.description = description;
       this.iconClass = iconClass;
+      this.url = url;
   }
 }
 
