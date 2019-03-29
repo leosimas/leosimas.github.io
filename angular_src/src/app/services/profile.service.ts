ea129353b3e10179ac1d569e802627fa7ee9ae01
Leonardo Simas <leonardo.simas@capgemini.com>
2019-03-29 10:24:05 -0300

    fixed: profileservice faz apenas 1 request do profile.json

--- angular_src/src/app/services/profile.service.ts
+++ angular_src/src/app/services/profile.service.ts
@@ -8,21 +8,34 @@ import { Profile } from '../models/profile';
 export class ProfileService {
 
   private profile: Profile;
+  private isLoading = false;
 
   constructor(private http: HttpClient) {
   }
 
   get(): Promise<Profile> {
     return new Promise<Profile>((resolve, reject) => {
+      if (this.isLoading) {
+        setTimeout(() => {
+          this.get()
+            .then(resolve)
+            .catch(reject);
+        }, 200);
+        return;
+      }
+
       if ( this.profile ) {
         resolve(this.profile);
         return;
       }
 
+      this.isLoading = true;
+
       this.http.get('/assets/json/profile.json').toPromise()
         .then((response) => {
           console.log('profile loaded:, ', response);
           this.profile = response as Profile;
+          this.isLoading = false;
           resolve(this.profile);
         })
         .catch(reject);
