94679092030b50b0b70b39549dbdad814e1ca95a
Leonardo Simas <leonardo.simas@capgemini.com>
2020-10-08 14:03:01 -0300

    fixed ios certificate url

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -16,9 +16,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "d77f8c5b43408d28c782c45252eeebf9",
-"/": "d77f8c5b43408d28c782c45252eeebf9",
-"main.dart.js": "6df735e4f3879045ee1ba48f01aa35cd",
+"index.html": "05aea3ec9d19cfd31f08a393cf630815",
+"/": "05aea3ec9d19cfd31f08a393cf630815",
+"main.dart.js": "37d4d93d6630ea5dda2719af64c92f3b",
 "manifest.json": "76f4067334e7f08b452aee562cba6360"
 };
 
