d1cc8b925800915673401d6bfc76ec138cab8c08
Leonardo Simas Gonçalves <leonardosg@ciandt.com>
2026-07-01 11:32:33 -0300

    new flutter updated version

--- angular_src/src/app/models/job.ts
+++
@@ -1,6 +0,0 @@
-export class Job {
-    title: string;
-    company: string;
-    period: string;
-    current?: boolean;
-}
--- angular_src/src/app/models/role.ts
+++
@@ -1,4 +0,0 @@
-export class Role {
-    title: string;
-    text: string;
-}
